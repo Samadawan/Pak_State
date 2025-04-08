b64ac2a8139f5538122325a367bd81612fc4c070
SAMAD MUSTAFA <Samadalvi2@gmail.com>
2025-04-08 21:55:11 +0500

    Complete Solution of Any State map in Gif

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ turtle.shape(image)
 data = pandas.read_csv("5_states.csv")
 all_states = data.state.to_list()
 Gussed_state =[]
+missing_states = []
 while len(Gussed_state) < len(all_states):
-    answer_state = screen.textinput(title=f"{len(Gussed_state)}/{len(all_states)} States Correct", prompt="Which one is next state").title()
+    answer_state = screen.textinput(title=f"{len(Gussed_state)}/{len(all_states)} States Correct",
+                                    prompt="Which one is next state").title()
+    if answer_state == "Exit":
+        for state in all_states:
+            if state not in Gussed_state:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States to learn.csv")
+        break
     if answer_state in all_states:
         Gussed_state.append(answer_state)
         t = turtle.Turtle()
@@ -22,4 +31,4 @@ while len(Gussed_state) < len(all_states):
 
 
 
-screen.exitonclick()
+
